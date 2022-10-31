a1b376814e928ce61fdfcdd2823939c14ce4093f
unknown <engr.mohammadjannatulnayeem@gmail.com>
2022-11-01 05:42:25 +0600

    code upload complete

---
+++ src/ArrayDemoProgramone/ArrayDemoProgramOne.java
@@ -0,0 +1,23 @@
+
+package ArrayDemoProgramone;
+
+/**
+ *
+ * @author nayeem
+ */
+public class ArrayDemoProgramOne {
+    public static void main(String[]args)
+    {
+     int[]myArray=new int[3];
+     myArray[0]=5;
+     myArray[1]=27;
+     myArray[2]=45;
+     
+     System.out.println(myArray[0]);
+     
+     System.out.println(myArray[0]+myArray[1]+myArray[2]);
+     
+     System.out.println(myArray.length);
+    
+    }
+}
